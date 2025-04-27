035cab7d0ca4838b46a16826ba50a4332b5d37ae
Michael <michael.zhu@sweet7.com>
2025-04-27 17:37:04 +0800

    feat: cut in formal bridge solution of view
    Signed-off-by: Michael <michael.zhu@sweet7.com>

--- lingxia-miniapp/src/miniapp/ipc.rs
+++ lingxia-miniapp/src/miniapp/ipc.rs
@@ -1,8 +1,7 @@
 // IPC script content included at compile time
-const IPC_SCRIPT: &str = include_str!("scripts/ipc.js");
+const IPC_SCRIPT: &str = include_str!("../../../lingxia-view/webview-bridge.js");
 
 /// Returns the IPC script content that should be injected into WebView
 pub fn get_ipc_script() -> &'static str {
     IPC_SCRIPT
 }
-
