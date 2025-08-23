6819fc18874d819ad64c60dc1bc107632dbcb6f3
michael <michael@lingxia.app>
2025-08-23 22:14:22 +0800

    chore(example:android): follow new sdk

--- examples/android/app/src/main/java/com/lingxia/example/miniapp/MainActivity.kt
+++ examples/android/app/src/main/java/com/lingxia/example/miniapp/MainActivity.kt
@@ -4,30 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.lingxia.lxapp.LxApp
-import com.lingxia.lxapp.LxAppActivity
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Initialize LxApp
         LxApp.initialize(this)
-
-        // Enable WebView debugging
         LxApp.enableWebViewDebugging()
-
-        // Configure transparent status bar using shared method
-        LxAppActivity.configureTransparentSystemBars(this)
-
-        // Open Home LxApp using the new method
         LxApp.openHomeLxApp()
-
         Log.d(TAG, "MainActivity initiated opening of home LxApp")
-
-        // Finish this activity since we're opening the home app in a new activity
-        // The home LxAppActivity will now function as our main activity
-        finish()
     }
 }
