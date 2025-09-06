72e42b48af3b801821d62998dcd01d48896c5891
michael <michael@lingxia.app>
2025-09-06 11:38:23 +0800

    refactor(lxapp): delete JS APIs that will be implemented in new crate

--- lingxia-lxapp/src/appservice/lx.rs
+++ lingxia-lxapp/src/appservice/lx.rs
@@ -1,9 +1,6 @@
 use rong::{JSContext, JSFunc, JSObject, JSResult};
 
-mod env;
 mod fastapi;
-mod navigator;
-mod system;
 
 pub(crate) use fastapi::get_fast_api;
 
@@ -20,14 +17,6 @@ pub use fastapi::{FastApiHandler, register_fast_api};
 pub(crate) fn init(ctx: &JSContext) -> JSResult<()> {
     let lx = JSObject::new(ctx);
     ctx.global().set("lx", lx.clone())?;
-
-    lx.set("env", env::init(ctx))?;
-
-    // Register navigator
-    let navigator_lxapp = JSFunc::new(ctx, navigator::navigator_to_lxapp)?;
-    register_js_api(ctx, "navigateToMiniProgram", navigator_lxapp)?;
-
-    system::init(ctx)?;
 
     Ok(())
 }
