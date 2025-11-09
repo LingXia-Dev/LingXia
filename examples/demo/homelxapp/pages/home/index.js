c8c84b03b60ffd889df9e09118e455906e54ba10
Michael <michael@lingxia.app>
2025-11-09 19:16:21 +0800

    feat(examples): Change imageUrl to path of LxApp's package
    Signed-off-by: Michael <michael@lingxia.app>

--- examples/demo/homelxapp/pages/home/index.js
+++ examples/demo/homelxapp/pages/home/index.js
@@ -4,7 +4,7 @@ const globalData = app.globalData;
 Page({
   data: {
     greeting: globalData.greeting,
-    imageUrl: "../../images/futuristic.jpg",
+    imageUrl: "images/futuristic.jpg",
     ipAddr: globalData.ipAddr,
     greetCount: 0,
   },
